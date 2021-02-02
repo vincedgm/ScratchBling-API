57575e775c4840eec9687905ebe0ea21a99fe3f8
Vince De Guzman <vince.012999@gmail.com>
2021-02-02 01:30:06 -0500

    Added controller

--- app/controllers/api/v1/backscratchers_controller.rb
+++ app/controllers/api/v1/backscratchers_controller.rb
@@ -1,2 +1,49 @@
 class Api::V1::BackscratchersController < ApplicationController
+    before_action :find_backscratcher, only: [:show, :update, :destroy]
+
+    def index
+        @backscratcher = Backscratcher.all
+        render json: @backscratcher
+    end
+
+    def show
+        render json: @backscratcher
+    end
+
+    def create
+        @backscratcher = Backscratcher.new(backscratcher_params)
+        if @backscratcher.save
+            render json: @backscratcher
+        else
+            render error: {error: 'Unable to create backscratcher.'}, status: 400
+        end
+    end
+
+    def update
+        if @backscratcher
+            @backscratcher.update(backscratcher_params)
+            render json: {message: 'Backscratcher updated.'}, status: 200
+        else
+            render json: {error: 'Unable to update backscratcher'}, status: 400
+        end
+    end
+
+    def destroy
+        if @backscratcher
+            @backscratcher.destroy
+            render json: {message: 'Backscratcher deleted.'}, status: 200
+        else
+            render json: {error: 'Unable to delete backscratcher'}, status: 400
+        end
+    end
+
+    private
+
+    def backscratcher_params
+        params.require(:backscratcher).permit(:name, :description, :size, :price)
+    end
+
+    def find_backscratcher
+        @backscratcher = Backscratcher.find(params[:id])
+    end
 end
