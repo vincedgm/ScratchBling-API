d7ee79e6db838c6f0bf4002b7523f9df075c4f83
Vince De Guzman <vince.012999@gmail.com>
2021-02-02 01:46:14 -0500

    Added seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Backscratcher.create([
+    {
+        name: 'The Itcher',
+        description: 'Scratch Any Itch',
+        size: ['XL'],
+        price: 27
+    },
+    {
+        name: 'The Blinger',
+        description: 'Diamonds',
+        size: ['L'],
+        price: 343
+    },
+    {
+        name: 'Glitz and Gold',
+        description: 'Gold handle and fancy emeralds',
+        size: ['XL','L','M','S'],
+        price: 4343
+    }
+])
