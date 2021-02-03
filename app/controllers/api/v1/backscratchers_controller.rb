aee79f7639370ea32720990a0574f7e1db9438b0
Vince De Guzman <vince.012999@gmail.com>
2021-02-02 20:55:54 -0500

    Formatting

--- app/controllers/api/v1/backscratchers_controller.rb
+++ app/controllers/api/v1/backscratchers_controller.rb
@@ -44,7 +44,7 @@ class Api::V1::BackscratchersController < ApplicationController
 
     private
     def backscratcher_params
-        params.require(:backscratcher).permit(:name, :description, :size, :price)
+        params.require(:backscratcher).permit(:name, :description, :price, :size)
     end
 
     def find_backscratcher
